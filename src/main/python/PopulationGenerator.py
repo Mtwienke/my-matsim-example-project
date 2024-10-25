1dbf77c80a65460bc40c4e5fd8953ccbe9d2bddd
Mtwienke <58438971+Mtwienke@users.noreply.github.com>
2024-10-25 14:59:22 +0200

    Runable code

--- src/main/python/PopulationGenerator.py
+++ src/main/python/PopulationGenerator.py
@@ -15,24 +15,41 @@ def generateNodeList(filePath):
 def createPopulation(size):
     
     nodeIdList = generateNodeList("/Users/markuswienkemeier/Desktop/Uni/BA_neu/MatSim/my-matsim-example-project/scenarios/Salzkotten/matsim-network.xml")
-
-    populationFile = open("/Users/markuswienkemeier/Desktop/Uni/BA_neu/MatSim/my-matsim-example-project/scenarios/Salzkotten/Population", "w")
-    populationFile.write('<?xml version="1.0" ?> \n <!DOCTYPE plans SYSTEM "http://www.matsim.org/files/dtd/plans_v4.dtd"> \n <plans xml:lang="de-CH"> \n')
+    
+    populationFilePath = "/Users/markuswienkemeier/Desktop/Uni/BA_neu/MatSim/my-matsim-example-project/scenarios/Salzkotten/Population.xml"
+    populationFile = open(populationFilePath, "w")
+    populationFile.write('<?xml version="1.0" ?> \n<!DOCTYPE plans SYSTEM "http://www.matsim.org/files/dtd/plans_v4.dtd"> \n')
     populationFile.close()
-    populationFile = open("/Users/markuswienkemeier/Desktop/Uni/BA_neu/MatSim/my-matsim-example-project/scenarios/Salzkotten/Population", "a")
+    populationFile = open(populationFilePath, "a")
+    
+    ET.register_namespace("xml", "http://www.w3.org/XML/1998/namespace")
+    root = ET.Element("plans", {"{http://www.w3.org/XML/1998/namespace}lang": "de"})
     
     for i in range(size):
-        person = ET.Element("person", id=str(i))
+        person = ET.SubElement(root, "person", id=str(i))
         plan = ET.SubElement(person, "plan")
         act = getRandomNode(nodeIdList, "home", plan, "6:00")
         act = getRandomNode(nodeIdList, "home", plan, "16:00")
         act = getRandomNode(nodeIdList, "home", plan, False)
         
-        rough_string = ET.tostring(person)
+        #rough_string = ET.tostring(population, encoding="unicode")
+        #populationFile.write(ET.tostring(person, encoding="unicode",))
+    formatted_xml_string = prettifyXML(root)
+    #formatted_without_root = '\n'.join(line for line in formatted_xml_string if line.strip() and "population" not in line)
+    finalXML = '\n'.join(formatted_xml_string)
+    populationFile.write(finalXML)
+    """populationFile.close()
+    populationFile = open(populationFilePath, "r")
+    parsedXML = minidom.parse(populationFilePath)
+    populationFile.close()
+    populationFile = open(populationFilePath, "w")
+    populationFile.write(parsedXML.toprettyxml())"""
         
-        populationFile.write(minidom.parseString(rough_string).toprettyxml(indent="    ", newl="\n"))
-        
-        
+def prettifyXML(element):
+    rough_string = ET.tostring(element, 'utf-8')
+    reparsed = minidom.parseString(rough_string)
+    # Generate pretty XML without the declaration
+    return reparsed.toprettyxml().splitlines()[1:]
                         
         
 def getRandomNode(NodeList, nodeType, parentElement, endTime):
